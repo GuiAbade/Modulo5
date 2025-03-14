dafb94b2dc40e87cd21f8ab7f9b99850c155b78d
Guilherme Abade <abade.guim13@gmail.com>
2025-03-14 15:55:25 -0300

    Manipulando Tuplas

---
+++ tuplas.py
@@ -0,0 +1,18 @@
+# Tuplas
+'''
+Para não precisar fazer: 
+  site1 = 'youtube.com'
+  site2 = 'facebook.com'
+  site3 = 'instagram.com'
+ou
+  valor1 = 23
+  valor2 = 43
+  valor3 = 65
+'''
+# Faça da seguinte forma:
+
+sites = ('youtube.com', 'facebook.com', 'instagram.com')
+valores = (23, 43, 65)
+# União de tuplas
+dados_do_sistema = sites + valores
+print(dados_do_sistema[2])
